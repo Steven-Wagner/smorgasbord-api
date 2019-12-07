f92f306cb53def282fef05b3feb3b778123bbedd
Steven Wagner <steven.wagner@adattivo.co>
2019-12-07 16:09:16 -0700

    Changed production env db variables

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
-  DB_URL:
-    process.env.DB_URL ||
-    "postgresql://smorgasbord:wabaxrmz42@localhost/smorgasborddb"
+  DATABASE_URL:
+    process.env.NODE_ENV === "production"
+      ? process.env.DATABASE_URL
+      : "postgresql://smorgasbord:wabaxrmz42@localhost/smorgasborddb"
 };
